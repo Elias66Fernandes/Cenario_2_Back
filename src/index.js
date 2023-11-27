092636b8d28c108412ddf10e1e2768d531234edf
Elias Fernandes <francisco.elias@discente.ufma.br>
2023-11-26 21:01:30 -0300

    Adicionando Funcionalidades

--- src/index.js
+++ src/index.js
@@ -24,12 +24,33 @@ app.post("/item", async (req, res) => {
   return res.sendStatus(201);
 });
 
-//rota para listar todos os usuários
+// rota para listar todos os usuários
 app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    console.error("Erro ao buscar usuários:", error);
+    res.status(500).send("Erro interno do servidor");
+  }
 });
 
-//rota para buscar um usuário pelo nome
+// rota para buscar um usuário pelo nome
 app.get("/user/:nome", async (req, res) => {
+  const { nome } = req.params;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { nome },
+    });
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).send("Usuário não encontrado");
+    }
+  } catch (error) {
+    console.error("Erro ao buscar usuário:", error);
+    res.status(500).send("Erro interno do servidor");
+  }
 });
 
 // Inicie o servidor na porta especificada
