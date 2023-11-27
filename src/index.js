e39a096389698a42ec9cffec9407d11bf9692bc4
Elias Fernandes <francisco.elias@discente.ufma.br>
2023-11-27 16:04:19 -0300

    Update

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
+//importações necessárias para o projeto
+require('dotenv').config();
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-const app = express();
-const port = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const app = express();
+const prisma = new PrismaClient();
+const port = process.env.PORT || 5000;
+
+//configurações para o servidor
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/users", async (req, res) => {
-  const users = await prisma.user.findMany();
-  if (users.length > 0) return res.status(200).send(users);
-  return res.status(404).send("No users found");
-});
-
+//rota para criar um item
 app.post("/user", async (req, res) => {
   const data = req.body;
   await prisma.user.create({
@@ -25,20 +24,36 @@ app.post("/user", async (req, res) => {
   return res.sendStatus(201);
 });
 
-app.get("/user/:name", async (req, res) => {
-  const name = req.params.name;
-  const user = await prisma.user.findMany({
-    where: {
-      name: name,
-    },
-  });
-  if (user.length > 0) return res.status(200).send(user);
-  return res.status(404).send("No user found");
+// rota para listar todos os usuários
+app.get("/users", async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.json(users);
+  } catch (error) {
+    console.error("Erro ao buscar usuários:", error);
+    res.status(500).send("Erro interno do servidor");
+  }
+});
+
+// rota para buscar um usuário pelo nome
+app.get("/user/:nome", async (req, res) => {
+  const { nome } = req.params;
+  try {
+    const user = await prisma.user.findUnique({
+      where: { nome },
+    });
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).send("Usuário não encontrado");
+    }
+  } catch (error) {
+    console.error("Erro ao buscar usuário:", error);
+    res.status(500).send("Erro interno do servidor");
+  }
 });
 
 // Inicie o servidor na porta especificada
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Servidor Express rodando na porta ${port}`);
 });
-
-module.exports = { app, server };
